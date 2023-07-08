ce946c2908c46c520dd14ccfedd64def4664a652
Rajesh Pandian <rajesh_pandian@outlook.com>
2023-07-08 12:52:34 +0100

    Committing deployment scripts

---
+++ main.tf
@@ -0,0 +1,17 @@
+resource "azurerm_resource_group" "static_website_rg" {
+  name     = "static-website-rg"
+  location = "UK South"
+}
+
+resource "azurerm_storage_account" "static_website_storage" {
+  name                     = "staticwebsite07082023"
+  resource_group_name      = azurerm_resource_group.static_website_rg.name
+  location                 = azurerm_resource_group.static_website_rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind             = "StorageV2"
+  static_website {
+    index_document     = "index.html"
+    error_404_document = "error.html"
+  }
+}
